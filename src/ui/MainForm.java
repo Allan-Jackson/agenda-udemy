c1f0071b237a6f067129121c567b4314859cb7af
Allan Jackson <allan.jackson.programmer@gmail.com>
2021-03-21 21:01:51 -0300

    listeners for the "Novo Contato" e "Cancelar" buttons added

--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -2,6 +2,9 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 public class MainForm extends JFrame{
 
@@ -23,11 +26,25 @@ public class MainForm extends JFrame{
         Point meioDaTela = new Point(dim.width/2 - getWidth()/2, dim.height/2 - getHeight()/2);
         //setLocation recebe um x e y ou então um objeto do tipo Point
         setLocation(meioDaTela);
-
+        //seta a operação que deve ser performada quando o frame for fechado
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+        setListeners();
     }
+    private void setListeners(){
+       buttonNewContact.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
+               new ContactForm(); //instancia o formulário de contato que, no seu construtor, constroi uma janela visível
+               dispose(); //fecha este formulário para que ele não fique sob o outro que foi aberto
+           }
+       });
+       buttonRemove.addActionListener(new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e) {
 
+           }
+       });
+    }
 
 
 }
